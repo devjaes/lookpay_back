c4c30727dd211254b4766aa834db3f24736a5d12
Jaesbyp <merajair17@gmail.com>
2023-05-24 21:51:15 -0500

    First commit

---
+++ src/main/java/dev/jeep/Lookpay/LookpayApplication.java
@@ -0,0 +1,13 @@
+package dev.jeep.Lookpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LookpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/jeep/Lookpay/LookpayApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jeep.Lookpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
